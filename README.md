afc7562aebca65ebba14e74ffffa4f962f0d6b7a
Dave Winer <dave.winer@gmail.com>
2020-01-13 12:49:39 -0500

    v0.5.18

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# daverss package
+# rss
 
 This is where I put my JavaScript code to generate RSS feeds from my apps, running in the browser and in Node. 
 
-### Story
+### Examples
 
-I need to document the data structure, however in the meantime, here are examples of the two data structures you have to send to `buildRssFeed` to get it to build an RSS feed for you:
+Here are examples of the two data structures you have to send to `buildRssFeed` to get it to build an RSS feed for you:
 
 1. <a href="https://github.com/scripting/rss/blob/master/examples/radio3/headElements.json">headElements</a>
 
 2. <a href="https://github.com/scripting/rss/blob/master/examples/radio3/historyArray.json">historyArray</a>
 
 These examples are from a snapshot of the data for my Radio3 linkblog feed, which is built using this code.
+
+And here's a Node test app that shows you how to call rss.buidRssFeed to create a feed locally.
 
 ### Updates
 
---
+++ example/test.js
@@ -0,0 +1,129 @@
+const fs = require ("fs");
+const rss = require ("daverss");
+
+var historyArray = [
+	{
+		"guid": {
+			"flPermalink": true,
+			"value": "http://www.niemanlab.org/2017/05/the-associated-press-is-adding-user-generated-social-content-verified-of-course-into-its-wire-services/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed:+NiemanJournalismLab+(Nieman+Journalism+Lab)"
+			},
+		"text": "The Associated Press is adding user-generated social content into its wire services.",
+		"title": "",
+		"link": "http://www.niemanlab.org/2017/05/the-associated-press-is-adding-user-generated-social-content-verified-of-course-into-its-wire-services/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed:+NiemanJournalismLab+(Nieman+Journalism+Lab)",
+		"linkShort": "http://bit.ly/2qoz8i6",
+		"whenLastEdit": "2017-05-16T17:11:14.375Z",
+		"flDirty": false,
+		"when": "2017-05-16T16:47:07.045Z",
+		"fname": "posts/0016043.json",
+		"idTweet": "864522615880380419",
+		"twitterScreenName": "davewiner",
+		"ctSends": 2,
+		"whenLastSend": "2017-05-16T17:10:39.178Z",
+		"idFacebookPost": "270259793181433_577437899130286",
+		"urlFacebookPost": "https://www.facebook.com/dave.winer.12/posts/577437899130286"
+		},
+	{
+		"guid": {
+			"flPermalink": true,
+			"value": "http://www.newyorker.com/news/john-cassidy/if-donald-trump-were-a-c-e-o-hed-probably-be-fired-today?mbid=rss"
+			},
+		"text": "If Donald Trump Were a C.E.O., He'd Probably Be Fired Today.",
+		"title": "",
+		"link": "http://www.newyorker.com/news/john-cassidy/if-donald-trump-were-a-c-e-o-hed-probably-be-fired-today?mbid=rss",
+		"linkShort": "http://bit.ly/2qoxxce",
+		"whenLastEdit": "2017-05-16T16:44:25.731Z",
+		"flDirty": false,
+		"when": "2017-05-16T16:44:25.731Z",
+		"fname": "posts/0016042.json",
+		"ctSends": 1,
+		"whenLastSend": "2017-05-16T16:44:26.761Z",
+		"idTweet": "864521938814857216",
+		"twitterScreenName": "davewiner",
+		"idFacebookPost": "270259793181433_577436092463800",
+		"urlFacebookPost": "https://www.facebook.com/dave.winer.12/posts/577436092463800"
+		},
+	{
+		"guid": {
+			"flPermalink": true,
+			"value": "http://www.americanthinker.com/articles/2017/05/the_mediademocratic_party_suicide_pact.html"
+			},
+		"text": "\"The national media and the Democratic Party have gone stark raving mad over President Trump's election victory.\"",
+		"title": "Articles: The Media-Democratic Party Suicide Pact",
+		"link": "http://www.americanthinker.com/articles/2017/05/the_mediademocratic_party_suicide_pact.html",
+		"linkShort": "http://bit.ly/2qo7Jga",
+		"whenLastEdit": "2017-05-16T15:15:41.455Z",
+		"flDirty": false,
+		"when": "2017-05-16T15:15:22.150Z",
+		"ctSends": 2,
+		"whenLastSend": "2017-05-16T15:15:42.122Z",
+		"fname": "posts/0016041.json",
+		"idTweet": "864499528577187840",
+		"twitterScreenName": "davewiner",
+		"idFacebookPost": "270259793181433_577400499134026",
+		"urlFacebookPost": "https://www.facebook.com/dave.winer.12/posts/577400499134026"
+		},
+	{
+		"guid": {
+			"flPermalink": true,
+			"value": "https://en.wikipedia.org/wiki/Baby_Huey"
+			},
+		"text": "El Presidente or Baby Huey?",
+		"title": "",
+		"link": "https://en.wikipedia.org/wiki/Baby_Huey",
+		"linkShort": "http://bit.ly/2rmsfen",
+		"whenLastEdit": "2017-05-16T15:11:04.882Z",
+		"flDirty": false,
+		"when": "2017-05-16T15:09:13.816Z",
+		"ctSends": 2,
+		"whenLastSend": "2017-05-16T15:11:05.193Z",
+		"fname": "posts/0016040.json",
+		"idTweet": "864497981080383488",
+		"twitterScreenName": "davewiner",
+		"idFacebookPost": "270259793181433_577399142467495",
+		"urlFacebookPost": "https://www.facebook.com/dave.winer.12/posts/577399142467495"
+		},
+	{
+		"guid": {
+			"flPermalink": true,
+			"value": "http://talkingpointsmemo.com/livewire/mitch-mcconnell-trump-drama?utm_content=buffer53d00&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer"
+			},
+		"text": "McConnell: Trump 'Drama' Distracts From Taxes, Deregulation, Health Care. #silverlining",
+		"title": "",
+		"link": "http://talkingpointsmemo.com/livewire/mitch-mcconnell-trump-drama?utm_content=buffer53d00&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer",
+		"linkShort": "http://bit.ly/2qo8iGS",
+		"whenLastEdit": "2017-05-16T15:04:20.583Z",
+		"flDirty": false,
+		"when": "2017-05-16T15:04:20.583Z",
+		"ctSends": 1,
+		"whenLastSend": "2017-05-16T15:04:21.237Z",
+		"fname": "posts/0016039.json",
+		"idTweet": "864496750853226497",
+		"twitterScreenName": "davewiner",
+		"idFacebookPost": "270259793181433_577397742467635",
+		"urlFacebookPost": "https://www.facebook.com/dave.winer.12/posts/577397742467635"
+		}
+	]
+var headElements = { 
+	"title": "Dave Winer",
+	"link": "http://scripting.com/",
+	"description": "I started up blogging, podcasting, RSS 2.0, and software for all of that. I love outliners, JavaScript. I love to make new media. Read my blog! :-)",
+	"language": "en-us",
+	"generator": "Radio3 v0.76d",
+	"docs": "http://cyber.law.harvard.edu/rss/rss.html",
+	"twitterScreenName": "davewiner",
+	"facebookPageName": "Scripting News",
+	"maxFeedItems": 25,
+	"appDomain": "radio3.io",
+	"flRssCloudEnabled": true,
+	"rssCloudDomain": "rpc.rsscloud.io",
+	"rssCloudPort": 5337,
+	"rssCloudPath": "/pleaseNotify",
+	"rssCloudRegisterProcedure": "",
+	"rssCloudProtocol": "http-post"
+	};
+
+var xmltext = rss.buildRssFeed (headElements, historyArray);
+fs.writeFile ("rss.xml", xmltext, function (err) {
+	});
+
+
